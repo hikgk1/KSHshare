7844288988a0bb515db14988b11f9565c2710332
hikgk1 <hikgk1@users.noreply.github.com>
2016-11-18 21:25:56 +0000

    Revert "Tilraun 2"
    This reverts commit 3fd91acc6dbe438c079ab0f048a6aa7f000bf07a.

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import project.EnvConfig;
+
 /**
  * The main class of the project.
  * By running the main class of {@link Application} then you start the Spring Boot system
