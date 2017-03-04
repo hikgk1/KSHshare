8cdda85d572007eacd9f62b7ca0b1d63463e3da1
hikgk1 <hikgk1@users.noreply.github.com>
2017-03-04 21:27:55 +0000

    Tilraun?

--- src/main/java/project/controller/UploadRestController.java
+++ src/main/java/project/controller/UploadRestController.java
@@ -23,22 +23,15 @@ public class UploadRestController {
 	}
 
 	@RequestMapping(value="/uploadr", method=RequestMethod.POST)
-	public UserImageContainer submit(@ModelAttribute("userImageContainer") UserImageContainer userImageContainer, Model model) {
+	public Object submit(@ModelAttribute("userImageContainer") UserImageContainer userImageContainer, Model model) {
 		String uuid = fileUploadService.store(userImageContainer);
-
-		System.out.println(uuid);
 
 		List<UserImageContainer> imgList = fileUploadService.findByUuid(uuid);
 
 		if(!imgList.isEmpty()) {
-			System.out.println("Fékk niðurstöðu");
-			System.out.println(imgList.get(0).getUuid());
 			UserImageContainer res = imgList.get(0);
-			System.out.println("Out of order?");
-			System.out.println(res.getUuid());
 			return res;
 		} else {
-			System.out.println("Fékk ekki niðurstöðu");
 			UserImageContainer res = new UserImageContainer();
 			res.setUuid("Error");
 			return res;
